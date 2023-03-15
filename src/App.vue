d2e54e6909163986420652f3f6c0e732e3ef474f
Andrew <hiagar11@gmail.com>
2023-03-15 12:19:51 +0300

    Начальная расфасовка файлов для лендинга

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+jg
+</template>
+
+<script>
+export default {
+  name: 'App',
+}
+</script>
+
+<style lang="scss">
+@import "bootstrap/dist/css/bootstrap-grid.min.css";
+@import "assets/styles/index";
+</style>
